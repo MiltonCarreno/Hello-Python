f84c78ef0ff33953b4f52df56c06565b2c8f20fc
Milton Carreno <milton@miltons-mbp.lan>
2024-08-28 22:44:06 -0400

    Variable Fun

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Hello-Python
+# Variable Fun
---
+++ Variables.py
@@ -0,0 +1,34 @@
+answer = "The answer to your query is "
+
+q1 = "5 multiplied by 6"
+a1 = 5 * 6
+print(q1)
+print(answer, a1)
+
+q2 = "\n8 divided by 3"
+a2 = 8 / 3
+print(q2)
+print(answer, "%.8f" % a2)
+
+q3 = "\n8 divided by 3 without the remainder"
+a3 = 8 // 3
+print(q3)
+print(answer, a3)
+
+q4 = "\n8 module 3 (remainder only)"
+a4 = 8 % 3
+print(q4)
+print(answer, a4)
+
+q5 = "\n4200000 add 23090"
+a5 = 4200000 + 23090
+print(q5)
+print(answer, a5)
+
+q6 = "\n4200000 subtract 7633"
+a6 = 4200000 - 7633
+print(q6)
+print(answer, a6)
+
+name = "\nMilton Carreno"
+print(name)
